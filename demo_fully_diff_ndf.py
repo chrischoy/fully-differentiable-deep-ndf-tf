5d71433373d2be68c5beb1719f3a5fd2c18a96b0
Chris Choy <chrischoy208@gmail.com>
2017-11-11 21:41:51 -0800

    Update demo_fully_diff_ndf.py

--- demo_fully_diff_ndf.py
+++ demo_fully_diff_ndf.py
@@ -2,54 +2,49 @@
 """
 # Fully Differentiable Deep Neural Decision Forest
 
-This is an implementation of a simple modification to the deep-neural decision
-forest [Kontschieder et al.] usng TensorFlow. The modification allows the joint
-optimization of decision nodes and leaf nodes which speeds up the training
-(haven't compared yet).
+[![DOI](https://zenodo.org/badge/20267/chrischoy/fully-differentiable-deep-ndf-tf.svg)](https://zenodo.org/badge/latestdoi/20267/chrischoy/fully-differentiable-deep-ndf-tf)
+
+This repository contains a simple modification of the deep-neural decision
+forest [Kontschieder et al.] in TensorFlow. The modification allows joint
+optimization of the decision nodes and leaf nodes which theoretically should speed up the training
+(haven't verified).
 
 
 ## Motivation:
 
-Deep Neural Deicision Forest, ICCV 2015, proposed a great way to incorporate a
-neural network with a decision forest. During the optimization (training), the
-terminal (leaf) node has to be updated after each epoch.
+Deep Neural Deicision Forest, ICCV 2015, proposed an interesting way to incorporate a decision forest into a neural network.
 
-This alternating optimization scheme is usually slower than joint optimization
-since other variable that is not being optimized slows down the optimization.
+The authors proposed incorporating the terminal nodes of a decision forest as static probability distributions and routing probabilities using sigmoid functions. The final loss is defined as the usual cross entropy between ground truth and weighted average of the terminal probabilities (weights being the routing probabilities).
+
+As there are two trainable parameters, the authors used alternating optimization. They first fixed the terminal node probabilities and trained the base network (routing probabilities), then, fixed the network and optimized the terminal nodes. Such alternating optimization is usually slower than joint optimization since variables that are not being optimized slow down the optimization of the other variable.
+
+However, if we parametrize the terminal nodes using a parametric probability distribution, we can jointly train both terminal and decision nodes, and theoretically, can speed up the convergence.
 
 This code is just a proof-of-concept that
 
-1. one can train both decision nodes and leaf nodes $\pi$ jointly using
-parametric formulation of leaf node.
+1. One can train both decision nodes and leaf nodes $\pi$ jointly using parametric formulation of leaf (terminal) nodes.
 
-2. one can implement the idea in a symbolic math library very easily.
+2. It is easy to implement such idea in a symbolic math library.
 
 
 ## Formulation
 
-The leaf node probability can be parametrized using a $softmax(W_{leaf})$.
-i.e. let a vector $W_{leaf} \in \mathbb{R}^N$ where N is the number of classes.
-
-Then taking the soft max operation on W_{leaf} would give us
-
-$$
-softmax(W_{leaf}) = \frac{e^{-w_i}}{\sum_j e^{-w_j}}
-$$
-
-which is always in a simplex. Thus, without any constraint, we can parametrize
-the leaf nodes and can compute the gradient of $L$ w.r.t $W_{leaf}$. This
-allows us to jointly optimize both leaf nodes and decision nodes.
-
+The leaf node probability $p \in \Delta^{n-1}$ can be parametrized using an $n$ dimensional vector $w_{leaf}$ $\exists w_{leaf}$ s.t. $p = softmax(w_{leaf})$. Thus, we can compute the gradient of $L$ w.r.t $w_{leaf}$ as well and can jointly optimize the terminal nodes as well.
 
 ## Experiment
 
-I tested a simple (3 convolution + 2 fc) network for the experiment.
+I used a simple (3 convolution + 2 fc) network for this experiment. On the MNIST, it reaches 99.1% after 10 epochs.
 
-On the MNIST, the simple Deep-NDF got 99.5% accuracy on the test set after 100
-epochs of training. After just 10 epochs, it reaches 99.1% and after 100
-epochs, it reaches 99.5%
+## Slides
 
-The following is the number of epoch and training accuracy after each epoch.
+[SDL Reading Group Slides](https://docs.google.com/presentation/d/1Ze7BAiWbMPyF0ax36D-aK00VfaGMGvvgD_XuANQW1gU/edit?usp=sharing)
+
+
+## Reference
+
+[Kontschieder et al.] Deep Neural Decision Forests, ICCV 2015
+
+The following is the expected output: the number of epoch and corresponding test accuracy.
 ```
 0 0.955829326923
 1 0.979166666667
